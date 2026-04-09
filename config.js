07641a127f45784b82bb4d5040c8ce92d71dd51a
Nahom-maker <ntade19@gmail.com>
2026-04-09 18:47:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,6 @@ const config = {
   supabase: {
     url: process.env.SUPABASE_URL,
     anonKey: process.env.SUPABASE_ANON_KEY,
-    serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY,
   },
 };
 
